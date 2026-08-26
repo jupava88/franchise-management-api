1129c99d7ffe8160b32d2da7d65c10fa9e9783cf
Juan Pablo Valencia Gallego <juanpvalencia88@gmail.com>
2026-08-26 09:57:15 -0500

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/jupava88/franchisemanagement/FranchiseManagementApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jupava88.franchisemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseManagementApiApplication.class, args);
+	}
+
+}
